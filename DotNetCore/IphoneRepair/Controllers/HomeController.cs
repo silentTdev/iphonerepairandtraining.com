c4123a59588f67bc58841caa9cb8192cab0f5e84
masteroleary <webdevllc@gmail.com>
2017-03-13 13:32:43 -0700

    DotNetCore

--- DotNetCore/IphoneRepair/Controllers/HomeController.cs
+++ DotNetCore/IphoneRepair/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IphoneRepair.Controllers
+namespace iPhoneRepair.Controllers
 {
     public class HomeController : Controller
     {
--- DotNetCore/IphoneRepair/Program.cs
+++ DotNetCore/IphoneRepair/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace IphoneRepair
+namespace iPhoneRepair
 {
     public class Program
     {
