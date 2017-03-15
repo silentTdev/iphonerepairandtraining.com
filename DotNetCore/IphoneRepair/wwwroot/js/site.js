170235bcf2bbd24c54c43f1dd7538a3dc94f8575
masteroleary <webdevllc@gmail.com>
2017-03-14 22:58:24 -0700

    Typescript

--- DotNetCore/IphoneRepair/wwwroot/js/site.js
+++ DotNetCore/IphoneRepair/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿// Write your Javascript code.
+var Animal = (function () {
+    function Animal(species) {
+        this.species = species;
+    }
+    Animal.prototype.speak = function () {
+        $.each($('.class'), function () {
+            $(this).val('');
+        });
+        alert('I am a ss' + this.species);
+    };
+    return Animal;
+}());
---
+++ DotNetCore/iPhoneRepair/wwwroot/js/site.ts
@@ -0,0 +1,16 @@
+﻿class Animal {
+    species: string;
+
+    constructor(species: string) {
+        this.species = species;
+    }
+
+    speak()
+    {
+        $.each($('.class'), function ()
+        {
+            $(this).val('');
+        });
+        alert('I am a ss' + this.species);
+    }
+}
