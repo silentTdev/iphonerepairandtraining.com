1ebcb474602582fad484d36833977399fc9486bb
masteroleary <webdevllc@gmail.com>
2017-03-16 21:08:52 -0700

    migrating to .net core

--- DotNetCore/IphoneRepair/Controllers/HomeController.cs
+++ DotNetCore/IphoneRepair/Controllers/HomeController.cs
@@ -3,31 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace iPhoneRepair.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
+        [Route("/index.html")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        [Route("/aboutme.html")]
+        public IActionResult AboutMe()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
-        public IActionResult Contact()
+        [Route("/aboutus.html")]
+        public IActionResult AboutUs()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
-
-        public IActionResult Error()
+        [Route("/findus.html")]
+        public IActionResult FindUs()
+        {
+            return View();
+        }
+        [Route("/gaming.html")]
+        public IActionResult Gaming()
+        {
+            return View();
+        }
+        [Route("/repairs.html")]
+        public IActionResult Repairs()
+        {
+            return View();
+        }
+        [Route("/silenttsolutions.html")]
+        public IActionResult SilentTSolutions()
+        {
+            return View();
+        }
+        [Route("/training.html")]
+        public IActionResult Training()
+        {
+            return View();
+        }
+        [Route("/warranty.html")]
+        public IActionResult Warranty()
         {
             return View();
         }
